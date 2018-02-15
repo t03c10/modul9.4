c01e24ba183500038dee942805f2f5dee274aad5
pawelz <t03c10@gmail.com>
2018-02-15 21:21:12 +0100

    task corrected

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 
 
 function drawTree(height) {
-for (var i = 0; i < height; i++) {
-	var star='';
-	var space = '     ';
-	
-for (var j = 0; j <= i; j++) {
-		 star += '*'; }
-		 space -= ' ';
-		console.log(space + star);
+	for (var i = 0; i < height; i++) {
+		var star = '';
+		
+		for (var j = height - 1; j > i; j--) {
+			 star += ' '; 
 		}
+		for (k = 0; k <= (i * 2) ; k++) {
+			star += "*";
+		}
+		console.log(star);
 	}
-	
-	drawTree(5);
+}
+drawTree(10);
